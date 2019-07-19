c9b28a4811564de2978f93bb61202f443ecf6929
Gabriel <gabriel.borchardt@hotmail.com>
2019-07-19 20:07:37 -0300

    rotas

---
+++ src/screens/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+export default function App() {
+ return (
+   <View style={styles.container}>
+     <Card title="Teste card">
+       <Text>Open up App.js to start working on your app!</Text>
+     </Card>
+   </View>
+ );
+}
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   backgroundColor: '#fff',
+   alignItems: 'center',
+   justifyContent: 'center',
+ },
+});
